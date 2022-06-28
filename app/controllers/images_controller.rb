9f483933dd4eebf9d8fa8de29567b50e899e281f
Tomoya7001 <spmiwdar7001@gmail.com>
2022-06-28 12:27:02 +0000

    投稿機能

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -4,6 +4,10 @@ class ImagesController < ApplicationController
 private
 
  def post_params
-  params.require(:image).permit(:image)
+   params.require(:image).permit(:image)
+ end
+
+ def image_params
+   params.require(:image).permit(:image)
  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get '/' => "homes#top"
   get "/index" => "homes#index"
 
+  resources :images, only: [:index, :new]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
